5e7f5dbfd0f305754a99fc1c1ab57dcd804a07ea
reevesrs24 <reevesrs24@gmail.com>
2018-03-02 21:08:58 -0800

    Added src and dst

--- port_server.py
+++ port_server.py
@@ -10,16 +10,16 @@ def start_port_knock_server(pkt):
 	#if IP in pkt:
 	#	print "DST = " + str(pkt[IP].dst) + " SRC = " + str(pkt[IP].src)
 	
-	if TCP in pkt and pkt[IP].src not in ips:
+	if TCP in pkt and pkt[IP].src not in ips and str(pkt[IP].dst) == '172.16.96.65':
 		ips[pkt[IP].src] = []
 		ip_port_tup = ( time.time() , pkt[TCP].dport )
 		ips[pkt[IP].src].append(ip_port_tup)
 
-	elif TCP in pkt:
+	elif TCP in pkt and str(pkt[IP].dst) == '172.16.96.65':
 		ip_port_tup = ( time.time() , pkt[TCP].dport )
 		ips[pkt[IP].src].append(ip_port_tup)
 
-		print "IP = " + str(pkt[IP].src) + " Port = " + str(pkt[TCP].dport)
+		print "SRC = " + str(pkt[IP].src) + " DST = " + str(pkt[IP].dst) + " Port = " + str(pkt[TCP].dport)
     
 
 if __name__ == "__main__":
